fe7a630f8cc90451c9473c25093adad7db1adc0e
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-03 16:35:34 +0200

    wip

--- src/components/FriendEditor.jsx
+++ src/components/FriendEditor.jsx
@@ -1,56 +1,44 @@
 import React from 'react';
 
 
-export default function FriendEditor({
-  form,
-  addFriend,
-  updateFriend,
-  isEditing,
-  inputChange,
-}) {
-  const onNameChange = event => {
-    inputChange(event.target.value, 'nameValue');
-  };
+export default class FriendEditor extends React.Component {
+  onNameChange = event => {
+    this.props.inputChange(event.target.value, 'nameValue');
+  }
 
-  const onAgeChange = event => {
-    inputChange(event.target.value, 'ageValue');
-  };
+  onAgeChange = event => {
+    this.props.inputChange(event.target.value, 'ageValue');
+  }
 
-  const onFriendAdd = event => {
-    addFriend();
-  };
+  render() {
+    return (
+      <div className='sub-container'>
+        {
+          this.props.isEditing
+            ? <h3>Edit Friend</h3>
+            : <h3>Add a new friend!</h3>
+        }
 
-  const onFriendUpdate = event => {
-    updateFriend();
-  };
+        name:
+        <input
+          type="text"
+          value={this.props.form.nameValue}
+          onChange={this.onNameChange}
+        />
 
-  return (
-    <div className='sub-container'>
-      {
-        isEditing
-          ? <h3>Edit Friend</h3>
-          : <h3>Add a new friend!</h3>
-      }
+        age:
+        <input
+          type="text"
+          value={this.props.form.ageValue}
+          onChange={this.onAgeChange}
+        />
 
-      name:
-      <input
-        type="text"
-        value={form.nameValue}
-        onChange={onNameChange}
-      />
-
-      age:
-      <input
-        type="text"
-        value={form.ageValue}
-        onChange={onAgeChange}
-      />
-
-      {
-        isEditing
-          ? <button onClick={onFriendUpdate}>Update Friend!</button>
-          : <button onClick={onFriendAdd}>Add Friend!</button>
-      }
-    </div>
-  );
+        {
+          this.props.isEditing
+            ? <button onClick={this.props.updateFriend}>Update Friend!</button>
+            : <button onClick={this.props.addFriend}>Add Friend!</button>
+        }
+      </div>
+    );
+  }
 }
